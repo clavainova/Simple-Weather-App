e614119d7c392e36f99297804e1392bcda95b7ad
Clavain <clavainova@gmail.com>
2021-05-06 12:38:36 +0200

    styling, searchbar verification

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 /* 
-REPLACE BUTTONS WITH TOUCHABLEOPACITY SO YOU CAN STYLE THEM
-
 
 -------------INSTRUCTIONS----------------
 Une application météo, avec les fonctionnalités suivant
@@ -10,12 +8,12 @@ Une page recherche : saisie, option géolocalisation, et liste des résultats
 Une page favoris : liste des favoris
 
 
-
 -------------TO DO LIST----------------
-- REPLACE BUTTONS WITH TOUCHABLEOPACITY SO YOU CAN STYLE THEM
-- this gets forecast of your *current* location. needs to search it(?)
-- need to add: search, custom locations, display correct information
-- work out static storage
+- fix passing params from search to index
+- implemen town name searching + laoding coords? new api?
+- static storage needs work 
+- add to favourites button
+- load existing favourites?
 */
 
 
@@ -25,7 +23,7 @@ Une page favoris : liste des favoris
 
 import 'react-native-gesture-handler'; //navigation
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, Button, FlatList, ActivityIndicator, SafeAreaView, ScrollView, Alert, RefreshControl, StatusBar, PermissionsAndroid, TextInput, ViewStyle, TextStyle, TextInputProps } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert, ImageBackground, TouchableOpacity, Button, FlatList, ActivityIndicator, SafeAreaView, ScrollView, RefreshControl, StatusBar, PermissionsAndroid, TextInput, ViewStyle, TextStyle, TextInputProps } from 'react-native';
 import * as Location from 'expo-location';
 
 //pagination
@@ -46,15 +44,35 @@ import AsyncStorage from 'react-native';
 
 const image = { uri: "https://i.pinimg.com/originals/b8/0e/9f/b80e9fb1bc10d0c3f1aed0929152576f.png" };
 
+//-----------TRANSITIONS---------------
+
+
+const horizontalAnimation = {
+  cardStyleInterpolator: ({ current, layouts }) => {
+    return {
+      cardStyle: {
+        transform: [{
+          translateX: current.progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [layouts.screen.width, 0],
+          }),
+        }]
+      }
+    };
+  }
+};
+
+
 //-----------NAVIGATION/APP---------------
 
 
 const Stack = createStackNavigator();
 
 function App({ navigation }) {
+  //print page
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="index">
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="index">
         <Stack.Screen name="index" component={index} />
         <Stack.Screen name="search" component={search} />
         <Stack.Screen name="faves" component={faves} />
@@ -62,8 +80,6 @@ function App({ navigation }) {
     </NavigationContainer>
   );
 }
-
-
 //-----------PERMISSIONS---------------
 
 const requestLocationPermission = async () => {
@@ -111,7 +127,6 @@ const granted = await PermissionsAndroid.request(
         },
 */
 
-
 //storage object
 const storeIt = create(
   persist(
@@ -132,11 +147,18 @@ const faves = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.navbar}>
-        <Button style={styles.navbuttonselected} onPress={() => navigation.navigate('index')} title="index" />
-        <Button style={styles.navbutton} title="Search" onPress={() => navigation.navigate('search')} />
-        <Button style={styles.navbutton} onPress={() => console.log("you are already here")} title="Favourites" />
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('index')} >
+          <Image style={styles.navIcon} source={require('./assets/cloud.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('search')} >
+          <Image style={styles.navIcon} source={require('./assets/search.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('faves')} >
+          <Image style={styles.navIcon} source={require('./assets/star.png')} />
+        </TouchableOpacity>
       </View>
       <ImageBackground source={image} style={styles.image}>
+
       </ImageBackground>
     </SafeAreaView>
   );
@@ -146,27 +168,56 @@ const faves = ({ navigation }) => {
 const search = ({ navigation }) => {
   //add event listeners? i think?
   const [text, onChangeText] = React.useState("");
+  var userInput = ""; //this is the value of the town they want to find
+
+  //after the user enters the town name...
+  //see if their location is in the database
+  const searchForLocation = () => {
+    //if input is valid, proceed
+    if (userInput !== "" && /^[a-zA-Z]+$/.test(userInput)) {
+      //if the input is valid, pass it back to the index
+      navigation.navigate('index', { town: userInput });
+      return;
+    }
+    //if input is invalid...
+    console.log("invalid");
+    Alert.alert('Location must not contain numbers or special characters.');
+    return;
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.navbar}>
-        <Button style={styles.navbuttonselected} onPress={() => navigation.navigate('index')} title="index" />
-        <Button style={styles.navbutton} title="Search" onPress={() => console.log("you are already here")} />
-        <Button style={styles.navbutton} onPress={() => navigation.navigate('faves')} title="Favourites" />
-      </View><TextInput
-        onChangeText={onChangeText}
-        value={text}
-        style={styles.input}
-      />
-      <ImageBackground source={image} style={styles.image}>
-      </ImageBackground>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('index')} >
+          <Image style={styles.navIcon} source={require('./assets/cloud.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('search')} >
+          <Image style={styles.navIcon} source={require('./assets/search.png')} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('faves')} >
+          <Image style={styles.navIcon} source={require('./assets/star.png')} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.searchBar}>
+        <TextInput onChangeText={
+          (text) => {
+            userInput = text;
+            return;
+          }} defaultValue={text} style={styles.input} />
+        <TouchableOpacity style={styles.button} onPress={searchForLocation} >
+          <Text style={styles.buttonTxt}>Go</Text>
+        </TouchableOpacity>
+      </View>
 
+      <ImageBackground source={image} style={styles.image}>
+
+      </ImageBackground>
     </SafeAreaView>
   );
 }
 
 
-const index = ({ navigation }) => {
+const index = ({ navigation }, town) => {
 
   //these are hooks
   //they let you use and state react features without writing a class
@@ -174,24 +225,40 @@ const index = ({ navigation }) => {
   const [forecast, setForecast] = useState(null); //get forecast
   const [refreshing, setRefreshing] = useState(false); //refresh it
 
+  //!!!!!!!!!!!!!!!!!!!!!!PARAMETRE PASSING NOT WORKING
+  //test zone
+  console.log("town = " + JSON.stringify(town));
+  console.log(town)
+  //end test zone!!!!!!!!!!!!!!!!!!!!!!!!
+
   const loadForecast = async () => {
     setRefreshing(true); //make it refresh
 
-    const { status } = await Location.requestPermissionsAsync(); //get user's location
-    if (status !== 'granted') { //if it's not granted...
-      requestLocationPermission;
-      Alert.alert('Location could not be accessed. Check permissions.');
+    //if they haven't searched for a town, just load their location
+    if (JSON.stringify(town) === "{}") {
+      const { status } = await Location.requestPermissionsAsync(); //get user's location
+      if (status !== 'granted') { //if it's not granted...
+        requestLocationPermission;
+        Alert.alert('Location could not be accessed. Check permissions.');
+      }
+
+      let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true }); //wait to get location...
+
+      const response = await fetch(`${url}&lat=${location.coords.latitude}&lon=${location.coords.longitude}`); //get longitude and latitude
+      const data = await response.json(); //wait for response - it will return lat and long in json
+
+      if (!response.ok) { //if you don't get a good response
+        Alert.alert(`Error getting weather data from server: ${data.message}`);
+      } else {
+        setForecast(data); //set the forecast to the data you got
+      }
     }
 
-    let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true }); //wait to get location...
-
-    const response = await fetch(`${url}&lat=${location.coords.latitude}&lon=${location.coords.longitude}`); //get longitude and latitude
-    const data = await response.json(); //wait for response - it will return lat and long in json
-
-    if (!response.ok) { //if you don't get a good response
-      Alert.alert(`Error getting weather data from server: ${data.message}`);
-    } else {
-      setForecast(data); //set the forecast to the data you got
+    //if they have searched for a town, load their search (or try to)
+    else {
+      //code here
+      console.log("parameter passed");
+      setForecast("data"); //set the forecast to your data to finish it off
     }
 
     setRefreshing(false); //once you've got the indfo, can stop refreshing
@@ -214,15 +281,23 @@ const index = ({ navigation }) => {
   const current = forecast.current.weather[0];
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.navbar}>
-        <Button style={styles.navbuttonselected} onPress={() => console.log("you are already here")} title="index" />
-        <Button style={styles.navbutton} onPress={() => navigation.navigate('search')} title="Search" />
-        <Button style={styles.navbutton} onPress={() => navigation.navigate('faves')} title="Favourites" />
-      </View>
-      <ScrollView refreshControl={<RefreshControl onRefresh={() => { loadForecast() }} refreshing={refreshing} />} >
-        <Text style={styles.title}>Weather</Text>
+      <ImageBackground source={image} style={styles.image}>
 
-        <ImageBackground source={image} style={styles.image}>
+        <View style={styles.navbar}>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('index')} >
+            <Image style={styles.navIcon} source={require('./assets/cloud.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('search')} >
+            <Image style={styles.navIcon} source={require('./assets/search.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('faves')} >
+            <Image style={styles.navIcon} source={require('./assets/star.png')} />
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView refreshControl={<RefreshControl onRefresh={() => { loadForecast() }} refreshing={refreshing} />} >
+          <Text style={styles.title}>Weather</Text>
+
           <View style={styles.current}>
             <Image
               style={styles.largeIcon}
@@ -232,55 +307,51 @@ const index = ({ navigation }) => {
             />
             <Text style={styles.currentTemp}>{Math.round(forecast.current.temp)}°C</Text>
           </View>
-        </ImageBackground>
 
-        <Text style={styles.currentDescription}>{current.description}</Text>
+          <Text style={styles.currentDescription}>{current.description}</Text>
 
-        <View>
-          <ImageBackground source={image} style={styles.image}>
+          <View>
             <Text style={styles.subtitle}>Hourly Forecast</Text>
-          </ImageBackground>
-          <FlatList horizontal
-            data={forecast.hourly.slice(0, 24)}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={(hour) => {
-              const weather = hour.item.weather[0];
-              var dt = new Date(hour.item.dt * 1000);
-              return <View style={styles.hour}>
-                <Text>{dt.toLocaleTimeString().replace(/:\d+ /, ' ')}</Text>
-                <Text>{Math.round(hour.item.temp)}°C</Text>
-                <Image
-                  style={styles.smallIcon}
-                  source={{
-                    uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
-                  }}
-                />
-                <Text>{weather.description}</Text>
-              </View>
-            }}
-          />
-        </View>
-        <ImageBackground source={image} style={styles.image}>
-          <Text style={styles.subtitle}>Next 5 Days</Text>
-        </ImageBackground>
-        {forecast.daily.slice(0, 5).map(d => { //Only want the next 5 days
-          const weather = d.weather[0];
-          var dt = new Date(d.dt * 1000);
-          return <View style={styles.day} key={d.dt}>
-            <Text style={styles.dayTemp}>{Math.round(d.temp.max)}°C</Text>
-            <Image
-              style={styles.smallIcon}
-              source={{
-                uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
+            <FlatList horizontal
+              data={forecast.hourly.slice(0, 24)}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={(hour) => {
+                const weather = hour.item.weather[0];
+                var dt = new Date(hour.item.dt * 1000);
+                return <View style={styles.hour}>
+                  <Text>{dt.toLocaleTimeString().replace(/:\d+ /, ' ')}</Text>
+                  <Text>{Math.round(hour.item.temp)}°C</Text>
+                  <Image
+                    style={styles.smallIcon}
+                    source={{
+                      uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
+                    }}
+                  />
+                  <Text>{weather.description}</Text>
+                </View>
               }}
             />
-            <View style={styles.dayDetails}>
-              <Text>{dt.toLocaleDateString()}</Text>
-              <Text>{weather.description}</Text>
-            </View>
           </View>
-        })}
-      </ScrollView>
+          <Text style={styles.subtitle}>Next 7 Days</Text>
+          {forecast.daily.slice(0, 7).map(d => {
+            const weather = d.weather[0];
+            var dt = new Date(d.dt * 1000);
+            return <View style={styles.day} key={d.dt}>
+              <Text style={styles.dayTemp}>{Math.round(d.temp.max)}°C</Text>
+              <Image
+                style={styles.smallIcon}
+                source={{
+                  uri: `http://openweathermap.org/img/wn/${weather.icon}@4x.png`,
+                }}
+              />
+              <View style={styles.dayDetails}>
+                <Text>{dt.toLocaleDateString()}</Text>
+                <Text>{weather.description}</Text>
+              </View>
+            </View>
+          })}
+        </ScrollView>
+      </ImageBackground>
 
     </SafeAreaView>
   );
@@ -332,17 +403,21 @@ const styles = StyleSheet.create({
     fontWeight: '200',
     fontSize: 24,
     color: '#373d4a',
-    margin: 12
+    backgroundColor: 'rgba(118, 182, 192, 0.5)'
   },
   hour: {
     padding: 6,
-    flexDirection: 'row',
-    backgroundColor: '#fff'
+    flexDirection: 'column',
+    backgroundColor: 'rgba(118, 182, 192, 0.5)',
+    borderRightWidth: "2px",
+    borderRightColor: "#fff"
   },
   day: {
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(118, 182, 192, 0.5)',
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderBottomWidth: "2px",
+    borderBottomColor: "#fff"
   },
   dayDetails: {
     alignItems: 'center',
@@ -356,29 +431,53 @@ const styles = StyleSheet.create({
   },
   largeIcon: {
     width: 200,
-    height: 100
+    height: 100,
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    borderRadius: "15%"
   },
   smallIcon: {
     width: 100,
-    height: 100
+    height: 100,
+    // borderLeftWidth: "2px",
+    // borderLeftColor: "#000",
+    // marginLeft: "5px"
   },
   input: {
     backgroundColor: '#fff',
     margin: 20,
     height: 30,
     padding: 5,
-    borderWidth: 1
   },
   navbar: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-around',
-    padding: 10
+    padding: 10,
+    backgroundColor: 'rgba(118, 182, 192, 0.5)'
+  },
+  navIcon: {
+    width: 50,
+    height: 50,
+    padding: "5px"
   },
   image: {
     flex: 1,
     //resizeMode: 'cover',
     justifyContent: "center"
+  },
+  button: {
+    alignItems: "center"
+  },
+  searchBar: {
+    flexDirection: 'row'
+  },
+  buttonTxt: {
+    backgroundColor: '#373d4a',
+    color: '#fff',
+    margin: 20,
+    height: 30,
+    padding: 5,
+    borderRadius: 25
   }
 });
 
